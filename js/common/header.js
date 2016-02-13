46e3a4672230d9b34d0c24455b5b15d4725361aa
Alina <smaylik2410@gmail.com>
2016-02-13 16:33:54 +0200

    Fix basket size is localStorage is empty

--- js/common/header.js
+++ js/common/header.js
@@ -2,4 +2,8 @@ document.querySelector(".search-icon").addEventListener("click", function(event)
     document.querySelector(".search.collapsed").classList.remove("collapsed");
 });
 
-document.querySelector(".quantity").textContent = JSON.parse(localStorage.getItem("bag")).length;
+if(JSON.parse(localStorage.getItem("bag")) != null) {
+    document.querySelector(".quantity").textContent = JSON.parse(localStorage.getItem("bag")).length;
+} else {
+    document.querySelector(".quantity").textContent = 0;
+}
