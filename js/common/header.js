7e44acd5f3c0324a09efc75d052e0ecb25f5d95f
Alina <smaylik2410@gmail.com>
2016-02-13 15:11:59 +0200

    Update bascket size

--- js/common/header.js
+++ js/common/header.js
@@ -1,3 +1,5 @@
 document.querySelector(".search-icon").addEventListener("click", function(event){
     document.querySelector(".search.collapsed").classList.remove("collapsed");
 });
+
+document.querySelector(".quantity").textContent = JSON.parse(localStorage.getItem("bag")).length;
