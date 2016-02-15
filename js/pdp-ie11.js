127a29651f1217d66eee3cb693311bbbf2851953
Alina <smaylik2410@gmail.com>
2016-02-15 11:09:08 +0200

    Fix js for pdp and shop-cart pages for IE11

---
+++ js/pdp-ie11.js
@@ -0,0 +1,145 @@
+"use strict";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "products.json", true);
+xhr.send();
+
+xhr.addEventListener("error", function () {
+    alert(xhr.status + ": " + xhr.statusText);
+});
+
+function getBag() {
+    var bag = localStorage.getItem("bag");
+    if (!bag) {
+        bag = [];
+    } else {
+        bag = JSON.parse(bag);
+    }
+    return bag;
+}
+
+function contains(bag, candidate) {
+    return bag.some(function (item) {
+        return item.product.id == candidate.product.id && item.color == candidate.color && item.size == candidate.size;
+    })
+}
+xhr.addEventListener("load", function (event) {
+    let json = JSON.parse(xhr.responseText);
+
+    var category;
+    for (let i = 0; i < json.length; i++){
+        if(json[i].id == getQueryParams().categoryId){
+            category = json[i];
+        }
+    }
+    console.log(category);
+
+    // if (!category) {
+    //     window.location = "notFound.html";
+    // }
+
+    var product;
+    for(let i = 0; i < category.products.length; i++){
+        if(category.products[i].id == getQueryParams().productId){
+            product = category.products[i];
+        }
+    }
+
+    console.log(product);
+
+    var bagItem = {
+        product: product,
+        quantity: 1,
+        size: product.sizes[0],
+        color: product.colors[0]
+    };
+
+
+    var productEl = document.createElement("div");
+    productEl.classList.add("product");
+    productEl.innerHTML = '<div class="product-photos">' +
+            '<img class="big-photo" src="' + product.images[0]+ '" alt="">' +
+            '<div class="small-photo-wrap">' +
+                '<div class="small-photo-container">' +
+                    '<img class="small-photo" src="' + product.images[1] +'" alt="">' +
+                '</div>' +
+                '<div class="small-photo-container">' +
+                     '<img class="small-photo" src="' + product.images[2] + '" alt="">' +
+                 '</div>' +
+                 '<div class="small-photo-container">' +
+                    '<img class="small-photo" src="' + product.images[3] + '" alt="">' +
+                 '</div>' +
+            '</div>' +
+        '</div>' +
+
+        '<div class="product-description">' +
+            '<h2 class="product-name">' + product.name + '</h2>' +
+            '<p class="product-number">Article number: ' + product.articleNumber + '</p>' +
+            '<p class="product-price">€ ' + product.price + '</p>' +
+            '<p class="description-text">' + product.description + '</p>' +
+
+            '<div class="attribute">' +
+                '<h3>size</h3>' +
+            '</div>'+
+        '</div>' +
+
+        '<button class="buy">add to cart</button>';
+
+    product.sizes.forEach(function (size) {
+        var attributeEl = document.createElement("button");
+        attributeEl.classList.add("attribute-btn");
+        attributeEl.textContent = size;
+
+
+        attributeEl.addEventListener("click", function (event) {
+            bagItem.size = size;
+            var selectedSizeEl = productEl.querySelector(".attribute-btn.selected");
+            if (selectedSizeEl) {
+                selectedSizeEl.classList.remove("selected");
+            }
+            attributeEl.classList.add("selected");
+
+        });
+
+        productEl.querySelector(".attribute").appendChild(attributeEl);
+    });
+
+    productEl.querySelector(".attribute-btn").classList.add("selected");
+
+    document.querySelector("main").insertBefore(productEl, document.querySelector(".gallery"));
+
+    document.querySelector(".buy").addEventListener("click", function (event) {
+        document.querySelector(".buy").classList.add("success");
+        document.querySelector(".buy").textContent = "product added";
+
+        var bag = getBag();
+
+        if (!contains(bag, bagItem)) {
+            bag.push(bagItem);
+        }
+        localStorage.setItem("bag", JSON.stringify(bag));
+        document.querySelector(".quantity").textContent = bag.length;
+    });
+
+    let smallList = document.querySelectorAll(".small-photo");
+    let smallArray = Array.prototype.slice.call(smallList, 0);
+    smallArray.forEach(function (element) {
+        element.addEventListener("click", function (event) {
+            document.querySelector(".big-photo").src = this.src;
+        });
+    });
+});
+
+function getQueryParams() {
+    var result = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        result[pair[0]] = pair[1];
+    }
+
+    return result;
+}
+
+   
---
+++ js/shop-cart-ie11.js
@@ -0,0 +1,75 @@
+"use strict";
+
+function getGetOrdinal(n) {
+    let s = ["th", "st", "nd", "rd"];
+    let v = n % 100;
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+function updateTotal() {
+    document.querySelector(".total-sum span").textContent = bag.reduce(function (sum, item) {
+        return sum + item.product.price * item.quantity;
+    }, 0);
+}
+
+function updateOrderNowButton() {
+    document.querySelector(".buy").disabled = !(bag && bag.length);
+}
+
+var bag = JSON.parse(localStorage.getItem("bag"));
+if(bag) {
+    bag.forEach(function (item, index) {
+        var itemEl = document.createElement("div");
+        itemEl.classList.add("cart-item");
+        itemEl.innerHTML = '<div class="photo-wrapper">' +
+        '<img class="photo" src="' + item.product.images[0] + '" alt="">' +
+     '</div>' +
+            '<div class="column column1">' +
+                '<div class="description">' +
+                    '<h3 class="item-title">' + item.product.name + ' (' + getGetOrdinal(index + 1) + ')</h3>' +
+                    '<!--<span class="sequence-number">(3rt)</span>-->' +
+                    '<p class="order-number">Ref.' + item.product.articleNumber + '</p>' +
+                '</div>' +
+                '<span class="color">' + item.color + '</span>' +
+                ' <span class="size">' + item.size + '</span>' +
+                '<input class="quantity" type="number" value="' + item.quantity + '" min="1">' +
+            '</div>' +
+            '<div class="column column2">' +
+                '<span class="amount">€ <span>' + item.product.price * item.quantity + '</span></span>' +
+                '<a class="delete" href="#">x</a>' +
+            '</div>';
+
+        document.querySelector(".order-cart").insertBefore(itemEl, document.querySelector(".subtotal"));
+
+
+        itemEl.querySelector(".quantity").addEventListener("change", function (event) {
+            item.quantity = this.value;
+            itemEl.querySelector(".amount span").textContent = item.product.price * item.quantity;
+            localStorage.setItem("bag", JSON.stringify(bag));
+            updateTotal();
+        });
+
+        itemEl.querySelector(".delete").addEventListener("click", function (event) {
+            event.preventDefault();
+            bag.splice(index, 1);
+            localStorage.setItem("bag", JSON.stringify(bag));
+            itemEl.parentElement.removeChild(itemEl);
+            updateTotal();
+            document.querySelector(".quantity").textContent = bag.length;
+            if(!bag.length){
+                updateOrderNowButton();
+            }
+        });
+    });
+    updateTotal();
+
+}
+updateOrderNowButton();
+
+
+
+document.querySelector(".buy").addEventListener("click", function (event) {
+    localStorage.clear();
+    window.location = "thank-you.html";
+
+});
